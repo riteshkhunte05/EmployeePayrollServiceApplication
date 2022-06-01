8f2faca92c19713e07410e637b66e545f2f171e9
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-06-01 23:17:54 +0530

    Added: Creating a Employee Payroll Spring Project with Welcome message

---
+++ src/main/java/com/bridgelabz/empPayrollServiceApp/EmpPayrollServiceApp/EmpPayrollServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.empPayrollServiceApp.EmpPayrollServiceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpPayrollServiceAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee PayRoll Service Application");
+		SpringApplication.run(EmpPayrollServiceAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bridgelabz/empPayrollServiceApp/EmpPayrollServiceApp/EmpPayrollServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.empPayrollServiceApp.EmpPayrollServiceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpPayrollServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
