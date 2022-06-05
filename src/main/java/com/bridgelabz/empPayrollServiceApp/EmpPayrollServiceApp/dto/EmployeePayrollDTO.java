43564ed9d9c98783524eaf3e8dde4782217487e3
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-06-05 18:20:41 +0530

    Added: Add Validation to name field with create and update REST calls too

--- src/main/java/com/bridgelabz/empPayrollServiceApp/EmpPayrollServiceApp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/empPayrollServiceApp/EmpPayrollServiceApp/dto/EmployeePayrollDTO.java
@@ -2,12 +2,19 @@ package com.bridgelabz.empPayrollServiceApp.EmpPayrollServiceApp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 /**
  * Employee Payroll DTO class.
  */
 @Data
 public class EmployeePayrollDTO {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name is Invalid")
+    @NotEmpty(message = "Employee name can't be null")
     public String name;
+    @Min(value = 500, message = "Min wage should be more than 500")
     public long salary;
 
     public EmployeePayrollDTO(String name, long salary) {
