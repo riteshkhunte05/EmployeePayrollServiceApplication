bd13be4c6bb3e955fe1b6ebb5292caf626b583c6
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-06-07 00:49:52 +0530

    Added: Validators added to All the attributes and both create abd Update Rest Calls

--- src/main/java/com/bridgelabz/empPayrollServiceApp/EmpPayrollServiceApp/exceptions/EmployeePayrollExceptionalHandler.java
+++ src/main/java/com/bridgelabz/empPayrollServiceApp/EmpPayrollServiceApp/exceptions/EmployeePayrollExceptionalHandler.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.empPayrollServiceApp.EmpPayrollServiceApp.exceptions;
 
 import com.bridgelabz.empPayrollServiceApp.EmpPayrollServiceApp.dto.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,7 +14,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionalHandler {
+
+    private static final String message = "Exception While Processing REST Request";
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO> handelHttpMessageNotReadableException(
+            HttpMessageNotReadableException exception) {
+        log.error("Invalid Date Format", exception);
+        ResponseDTO respDTO = new ResponseDTO(message,"Should have date in dd MMM yyyy format");
+        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
